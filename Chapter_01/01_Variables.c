1ad7d82aca4da022377752495bb474d3aa2df6ca
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-08-17 12:52:20 +0530

    Chapter_01

---
+++ Chapter_01/01_Variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    // int a; 
+    // a = 6;
+    // printf("The Value of a is %d", a);
+
+    // Rules of variables 
+    int a;
+    int Ujjawal;
+    int ujjawal_Thakur;
+    // int 7; // Constant not allowed
+    int $ujjawal;
+    $ujjawal = 3;
+    printf("%d", $ujjawal);
+    return 0;
+}
