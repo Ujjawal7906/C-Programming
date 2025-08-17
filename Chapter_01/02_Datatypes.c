887d1dcd95df8eaa0de1ee661dbbc3249b9eb6cb
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-08-17 13:29:09 +0530

    Datatypes

--- Chapter_01/02_Datatypes.c
+++ Chapter_01/02_Datatypes.c
@@ -8,5 +8,9 @@ int main(){
 
     char c;
     c = 'g'; // Character datatype
+
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %f \n", b);
+    printf("The value of c is %c \n", c);
     return 0;
 }
