9d97109687578a95f8b054cc2babe7eb95c3414a
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-08-17 13:21:48 +0530

    Datatypes

---
+++ Chapter_01/02_Datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a;
+    a = 5; // Integer datatype
+
+    float b;
+    b=7.6; // Float datatype
+
+    char c;
+    c = 'g'; // Character datatype
+    return 0;
+}
