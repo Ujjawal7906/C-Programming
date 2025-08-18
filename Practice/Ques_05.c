14c8bc7f31f79948ba94e4900029f2f4e212038c
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-08-18 12:55:29 +0530

    Programs

---
+++ Practice/Ques_05.c
@@ -0,0 +1,9 @@
+// What data type will 3.0/8 – 2 return
+#include <stdio.h>
+int main(){
+    float a = 3.0;
+    int b = 8;
+    int c = 2;
+    printf("The value of 3.0/8-2 is %f", a/b-c); // float
+    return 0;
+}
