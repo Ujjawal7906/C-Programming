1e9e922cced18d1473b7c6cd8a07461c110731dd
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-08-18 15:18:20 +0530

    Practice

---
+++ Practice/Ques_06.c
@@ -0,0 +1,38 @@
+/*Write a program to find grade of a student given his marks based on below:
+    90 – 100 => A
+    80 – 90 => B
+    70 – 80 => C
+    60 – 70 => D
+    50 – 60 => E
+    <50 => F*/
+
+    #include <stdio.h>
+    int main(){
+        char grade;
+        int marks;
+        printf("Enter your marks: ");
+        scanf("%d", &marks);
+
+        if (marks >= 90 && marks <= 100){
+            printf("Grade A");
+        }
+
+        else if(marks >= 80 && marks < 90){
+            printf("Grade B");
+        }
+
+        else if(marks >= 70 && marks < 80){
+            printf("Grade C");
+        }
+        else if(marks >= 60 && marks < 70){
+            printf("Grade D");
+        }
+        else if(marks >= 50 && marks < 60){
+            printf("Grade E");
+        }
+        
+        else{
+            printf("Grade F");
+        }
+        return 0;
+    }
