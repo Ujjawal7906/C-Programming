005f88bdff56c2efade9695b26959de225a5fec2
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-08-18 13:31:17 +0530

    Chapter 2

---
+++ Chapter_02/01_arithematic-operator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a = 2;
+    int b = 3;
+    int c = a + b;
+    int d = a - b;
+    int e = a*b;
+    int f = a/b;
+    int g = a%b;
+    printf("The value of c is %d\n", c);
+    printf("The value of d is %d\n", d);
+    printf("The value of e is %d\n", e);
+    printf("The value of f is %d\n", f);
+    printf("The value of g is %d\n", g);
+    return 0;
+}
---
+++ Chapter_02/03_typecasting.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a;
+    float b = 34.55;
+    a = (int) b;
+    printf("The value of a is %d", a);
+    return 0;
+}
